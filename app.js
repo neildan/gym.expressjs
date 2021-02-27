31ac19062b6ea76c8fd054ab63dccc9572faf107
neildan <44823148+neildan@users.noreply.github.com>
2021-02-27 18:27:08 -0500

    Add cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
+const cors = require("cors")
 const apiRouter = require('./routes/api')
 const app = express()
 
 require('./db')
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 app.use('/api', apiRouter)
