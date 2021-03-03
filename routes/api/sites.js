aea23a4b19bf05375810b7da03e729bcb1a8ea4c
neildan <44823148+neildan@users.noreply.github.com>
2021-03-03 13:52:06 -0500

    Add a query that get a list of all users that belong to a site

--- routes/api/sites.js
+++ routes/api/sites.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Site, City } = require('../../db')
+const { Site, City, User } = require('../../db')
 const { check, validationResult } = require('express-validator')
 const trait = require('../trait')
 
@@ -20,6 +20,20 @@ router.get('/:siteId', async(req, res) => {
         if (!site) throw { data: {}, msj: 'La sede no existe' }
 
         res.json(trait.success(site, 'ok'))
+    } catch (e) {
+        res.json(trait.error(e.data, e.msj))
+    }
+})
+
+/**
+ * Get all users of a site
+ */
+router.get('/users/:siteId', async(req, res) => {
+    try {
+        let users = await User.findAll({ where: { siteId: req.params.siteId } })
+        if (!users) throw { data: {}, msj: 'No hay usuarios en esa sede' }
+
+        res.json(trait.success(users, 'ok'))
     } catch (e) {
         res.json(trait.error(e.data, e.msj))
     }
